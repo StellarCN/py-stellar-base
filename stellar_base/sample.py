0a98d5bfc836c6d6eaaf767eaf55a161da465eed
Akuukis <redirected@kalvis.lv>
2015-11-03 18:25:03 +0200

    Improved sample to also announce

--- stellar_base/sample.py
+++ stellar_base/sample.py
@@ -1,38 +1,44 @@
 # coding:utf-8
+import requests
+import simplejson as json
+from urllib.parse import urlencode
+
 import base64
 import os, sys
 from stellar_base.stellarxdr import StellarXDR_pack as Xdr
-from stellar_base.operation import Operation
+from stellar_base.operation import PaymentOperation
 from stellar_base.asset import Asset
 from stellar_base.transaction import Transaction
-from stellar_base.transaction_envelopo import TransactionEnvelope as Te
-from stellar_base.keypair import KeyPair
+from stellar_base.transaction_envelope import TransactionEnvelope as Te
+from stellar_base.keypair import Keypair
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # base info
-seqNum = "34909494181888"
-address = "GDVDKQFP665JAO7A2LSHNLQIUNYNAAIGJ6FYJVMG4DT3YJQQJSRBLQDG"
-secret = 'SCVLSUGYEAUC4MVWJORB63JBMY2CEX6ATTJ5MXTENGD3IELUQF4F6HUB'
+seqNum = 2094810169081858 # Remember to increment it
+address = 'GDJVFDG5OCW5PYWHB64MGTHGFF57DRRJEDUEFDEL2SLNIOONHYJWHA3Z'
+secret = 'SAHPFH5CXKRMFDXEIHO6QATHJCX6PREBLCSFKYXTTCDDV6FJ3FXX4POT'
+
 # input
-destination = 'GBS43BF24ENNS3KPACUZVKK2VYPOZVBQO2CISGZ777RYGOPYC2FT6S3K'
+destination = 'GCW24FUIFPC2767SOU4JI3JEAXIHYJFIJLH7GBZ2AVCBVP32SJAI53F5'
 asset = Asset.native()
-amount = 1000
+amount = 10000000
 
 # process
 opts = {'source': address, 'destination': destination, 'asset': asset, 'amount': amount}
-operation = Operation.payment(opts)
+operation = PaymentOperation(opts)
 
-
-opts = {'seqNum': seqNum}
-
-tx = Transaction(address, opts)
+tx = Transaction(address, {'seqNum': seqNum, 'fee': 100})
 tx.add_operation(operation)
 envelope = Te(tx)
-signer = KeyPair.from_seed(secret)
+signer = Keypair.from_seed(secret)
 envelope.sign(signer)
 exo = envelope.to_xdr_object()
 x = Xdr.STELLARXDRPacker()
 x.pack_TransactionEnvelope(exo)
 ex = x.get_buffer()
 ex = base64.b64encode(ex)
-print(ex)
+
+url = "https://horizon-testnet.stellar.org/transactions"
+#url = "http://posttestserver.com/post.php"
+r = requests.post(url, data={'tx': ex.decode('ascii')})
+print(r.text)
