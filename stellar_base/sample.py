fa9b887573ef84e186994dc5e76cf4d2a0147f45
Akuukis <redirected@kalvis.lv>
2015-11-09 21:46:52 +0200

    Pull upstream/master

--- stellar_base/sample.py
+++ stellar_base/sample.py
@@ -1,8 +1,4 @@
 # coding:utf-8
-import requests
-import simplejson as json
-from urllib.parse import urlencode
-
 import base64
 import os, sys
 from stellar_base.stellarxdr import StellarXDR_pack as Xdr
@@ -14,20 +10,22 @@ from stellar_base.keypair import Keypair
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # base info
-seqNum = 2094810169081858 # Remember to increment it
-address = 'GDJVFDG5OCW5PYWHB64MGTHGFF57DRRJEDUEFDEL2SLNIOONHYJWHA3Z'
-secret = 'SAHPFH5CXKRMFDXEIHO6QATHJCX6PREBLCSFKYXTTCDDV6FJ3FXX4POT'
-
+seqNum = "34909494181888"
+address = "GDVDKQFP665JAO7A2LSHNLQIUNYNAAIGJ6FYJVMG4DT3YJQQJSRBLQDG"
+secret = 'SCVLSUGYEAUC4MVWJORB63JBMY2CEX6ATTJ5MXTENGD3IELUQF4F6HUB'
 # input
-destination = 'GCW24FUIFPC2767SOU4JI3JEAXIHYJFIJLH7GBZ2AVCBVP32SJAI53F5'
+destination = 'GBS43BF24ENNS3KPACUZVKK2VYPOZVBQO2CISGZ777RYGOPYC2FT6S3K'
 asset = Asset.native()
-amount = 10000000
+amount = 1000
 
 # process
 opts = {'source': address, 'destination': destination, 'asset': asset, 'amount': amount}
 operation = PaymentOperation(opts)
 
-tx = Transaction(address, {'seqNum': seqNum, 'fee': 100})
+
+opts = {'seqNum': seqNum}
+
+tx = Transaction(address, opts)
 tx.add_operation(operation)
 envelope = Te(tx)
 signer = Keypair.from_seed(secret)
@@ -37,7 +35,4 @@ x = Xdr.STELLARXDRPacker()
 x.pack_TransactionEnvelope(exo)
 ex = x.get_buffer()
 ex = base64.b64encode(ex)
-
-url = "https://horizon-testnet.stellar.org/transactions"
-r = requests.post(url, data={'tx': ex.decode('ascii')})
-print(r.text)
+print(ex)
