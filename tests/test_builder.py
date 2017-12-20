1c91e1238970a3c9a62db12910db1214e01ca1c4
overcat <4catcode@gmail.com>
2017-12-20 23:28:07 +0800

    handle tx_bad_seq in tests

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -19,7 +19,6 @@ class TestBuilder(object):
     def test_builder(self):
         cold_account = self.cold.address().decode()
         hot_account = self.hot.address().decode()
-
         fund(cold_account)
 
         cold = Builder(self.cold.seed().decode())
@@ -27,10 +26,10 @@ class TestBuilder(object):
         cold.append_create_account_op(hot_account, 200)
 
         cold.append_set_options_op(inflation_dest=cold_account, set_flags=1,
-                                   home_domain='256kw.com',master_weight=10,
+                                   home_domain='256kw.com', master_weight=10,
                                    low_threshold=5,)
         cold.append_trust_op(cold_account, 'BEER', 1000, source=hot_account)
-        cold.append_allow_trust_op(hot_account,'BEER', True)
+        cold.append_allow_trust_op(hot_account, 'BEER', True)
         # append twice for test
         cold.append_payment_op(hot_account, 50.123, 'BEER', cold_account)
         cold.append_payment_op(hot_account, 50.123, 'BEER', cold_account)
@@ -39,7 +38,7 @@ class TestBuilder(object):
         try:  # sign twice
             cold.sign(self.hot.seed().decode())
         except SignatureExistError:
-            assert True 
+            assert True
         except:
             assert False
 
@@ -57,12 +56,14 @@ class TestBuilder(object):
         cold_account = self.cold.address().decode()
         hot_account = self.hot.address().decode()
         fund(cold_account)
+
         fund(hot_account)
 
         cold = Builder(self.cold.seed().decode())
         cold.append_trust_op(cold_account, 'BEER', 1000, hot_account)
         cold.append_payment_op(hot_account, 100, 'BEER', cold_account)
-        cold.append_payment_op(cold_account, 2.222, 'BEER', cold_account, hot_account)
+        cold.append_payment_op(cold_account, 2.222,
+                               'BEER', cold_account, hot_account)
         xdr = cold.gen_xdr()
         hot = Builder(self.hot.seed().decode())
         hot.import_from_xdr(xdr)
@@ -75,11 +76,14 @@ class TestBuilder(object):
 
 
 def fund(address):
-    r = requests.get(HORIZON_TEST + '/friendbot?addr=' + address)  # Get 10000 lumens
-    t = r.text
-    if 'hash' in json.loads(t):
-        return True
-    elif 'op_already_exists' in json.loads(t):
-        return True
-    else:
-        return False
+    for attempt in range(3):
+        r = requests.get(HORIZON_TEST + '/friendbot?addr=' +
+                         address)  # Get 10000 lumens
+        t = r.text
+        try:
+            assert 'hash' in json.loads(
+                t) or 'op_already_exists' in json.loads(t)
+            return True
+        except AssertionError:
+            pass
+    raise Exception("fund failed")
--- tests/test_horizon.py
+++ tests/test_horizon.py
@@ -1,11 +1,14 @@
+# encoding: utf-8
 import requests
 import json
 from stellar_base.keypair import Keypair
 from stellar_base.memo import *
 from stellar_base.operation import *
-
+from stellar_base.builder import HORIZON_TEST
 from stellar_base.horizon import Horizon
+
 horizon = Horizon()
+
 
 class TestMethods:
     def __init__(self):
@@ -14,8 +17,7 @@ class TestMethods:
         self.address = kp.address().decode('ascii')
         self.seed = kp.seed().decode('ascii') or None
 
-        r = requests.get('https://horizon-testnet.stellar.org/friendbot?addr=' + self.address)
-        assert 'hash' in json.loads(r.text), "\n" + urlParam + "\n" + r.text
+        fund(self.address)
 
     def make_envelope(self, *args, **kwargs):
         from stellar_base.transaction import Transaction
@@ -27,21 +29,35 @@ class TestMethods:
         }
         for opt, value in kwargs.items():
             opts[opt] = value
-        tx = Transaction( source=self.address, opts=opts)
+        tx = Transaction(source=self.address, opts=opts)
         for count, op in enumerate(args):
-            tx.add_operation( operation=op )
-        envelope = Te( tx=tx, opts={"network_id": "TESTNET"} )
-        signer = Keypair.from_seed( seed=self.seed )
-        envelope.sign( keypair=signer )
+            tx.add_operation(operation=op)
+        envelope = Te(tx=tx, opts={"network_id": "TESTNET"})
+        signer = Keypair.from_seed(seed=self.seed)
+        envelope.sign(keypair=signer)
         envelope_xdr = envelope.xdr()
         print(envelope_xdr)
         return envelope_xdr
 
     def test_submit(self):
         envelope_xdr = self.make_envelope(Payment({
-                'destination': self.address,
-                'asset': Asset.native(),
-                'amount': "0.0001618"
+            'destination': self.address,
+            'asset': Asset.native(),
+            'amount': "0.0001618"
         }))
-        response = horizon.submit( envelope_xdr )
+        response = horizon.submit(envelope_xdr)
         assert 'hash' in response
+
+
+def fund(address):
+    for attempt in range(3):
+        r = requests.get(HORIZON_TEST + '/friendbot?addr=' +
+                         address)  # Get 10000 lumens
+        t = r.text
+        try:
+            assert 'hash' in json.loads(
+                t) or 'op_already_exists' in json.loads(t)
+            return True
+        except AssertionError:
+            pass
+    raise Exception("fund failed")
