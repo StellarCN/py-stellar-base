7acefdab050338f6c057d6fa2edf68cc8d89c85c
overcat <4catcode@gmail.com>
2019-07-29 23:33:21 +0800

    feat: account

---
+++ stellar_sdk/account.py
@@ -0,0 +1,19 @@
+from .strkey import StrKey
+
+
+class Account:
+    def __init__(self, account_id: str, sequence: int) -> None:
+        StrKey.is_valid_ed25519_public_key(account_id)
+
+        self.account_id = account_id
+        self.sequence = sequence
+
+    def increment_sequence_number(self) -> None:
+        self.sequence += 1
+
+    def __eq__(self, other: 'Account'):
+        return self.account_id == other.account_id and self.sequence == other.sequence
+
+    def __str__(self):
+        return '<Account [account_id={account_id}, sequence={sequence}]>'.format(account_id=self.account_id,
+                                                                                 sequence=self.sequence)
