294624b9f3befb3779e22b613fc6b881cbadfd8e
Jun Luo <4catcode@gmail.com>
2019-07-31 20:09:26 +0800

    feat: Asset

--- stellar_sdk/asset.py
+++ stellar_sdk/asset.py
@@ -1,56 +1,37 @@
-# coding: utf-8
-
-import base64
 import re
 
-from stellar_base.exceptions import AssetCodeInvalidError, StellarAddressInvalidError
-from .utils import account_xdr_object, encode_check, is_valid_address
+from .keypair import Keypair
+from .strkey import StrKey
+from .exceptions import AssetCodeInvalidError, AssetIssuerInvalidError
 from .stellarxdr import Xdr
 
 
-class Asset(object):
-    """The :class:`Asset` object, which represents an asset and its
-    corresponding issuer on the Stellar network.
-
-    For more information about the formats used for asset codes and how issuers
-    work on Stellar's network, see `Stellar's guide on assets`_.
-
-    :param str code: The asset code, in the formats specified in `Stellar's
-        guide on assets`_.
-    :param issuer: The strkey encoded issuer of the asset. Note if the
-        currency is the native currency (XLM (Lumens)), no issuer is necessary.
-    :type issuer: str, None
-
-    .. _Stellar's guide on assets:
-        https://www.stellar.org/developers/guides/concepts/assets.html
-
-    """
-
-    _ASSET_CODE_RE = re.compile(r'^[a-zA-Z0-9]{1,12}$')
-
+class Asset:
     def __init__(self, code, issuer=None):
-        if not self._ASSET_CODE_RE.match(code):
-            raise AssetCodeInvalidError("Asset code is invalid (alphanumeric, 12 "
-                                        "characters max).")
+        asset_code_re = re.compile(r'^[a-zA-Z0-9]{1,12}$')
+        if not asset_code_re.match(code):
+            raise AssetCodeInvalidError('Asset code is invalid (maximum alphanumeric, 12 characters at max).')
 
-        if issuer is not None:
-            try:
-                assert is_valid_address(issuer)
-            except StellarAddressInvalidError:
-                raise StellarAddressInvalidError('Invalid issuer account: {}'.format(issuer))
+        if code != 'XLM' and issuer is None:
+            raise AssetIssuerInvalidError("The issuer cannot be `None` except for the native asset.")
 
-        if code.lower() != 'xlm' and issuer is None:
-            raise StellarAddressInvalidError("Issuer cannot be `None` except native asset.")
+        if issuer is not None and not StrKey.is_valid_ed25519_public_key(issuer):
+            raise AssetIssuerInvalidError("The issuer should be a correct public key.")
 
         self.code = code
         self.issuer = issuer
-        self.type = self.guess_asset_type()
+        self._type = self.guess_asset_type()
 
-    def __eq__(self, other):
-        return self.xdr() == other.xdr()
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, v):
+        raise AttributeError("Asset type is immutable.")
 
     def guess_asset_type(self):
-        if self.code.lower() == 'xlm' and self.issuer is None:
+        if self.code == 'XLM' and self.issuer is None:
             asset_type = 'native'
         elif len(self.code) > 4:
             asset_type = 'credit_alphanum12'
@@ -59,41 +40,20 @@ class Asset(object):
         return asset_type
 
     def to_dict(self):
-        """Generate a dict for this object's attributes.
-
-        :return: A dict representing an :class:`Asset`
-        """
-        rv = {'code': self.code}
+        rv = {'type': self.type}
         if not self.is_native():
+            rv['code'] = self.code
             rv['issuer'] = self.issuer
-            rv['type'] = self.type
-        else:
-            rv['type'] = 'native'
         return rv
 
     @staticmethod
     def native():
-        """Create a :class:`Asset` with the native currency.
-
-        Currently, the native currency is Stellar Lumens (XLM)
-
-        :return: A new :class:`Asset` representing the native currency on the
-            Stellar network.
-        """
         return Asset("XLM")
 
     def is_native(self):
-        """Return true if the :class:`Asset` is the native asset.
-
-        :return: True if the Asset is native, False otherwise.
-        """
         return self.issuer is None
 
     def to_xdr_object(self):
-        """Create an XDR object for this :class:`Asset`.
-
-        :return: An XDR Asset object
-        """
         if self.is_native():
             xdr_type = Xdr.const.ASSET_TYPE_NATIVE
             return Xdr.types.Asset(type=xdr_type)
@@ -102,7 +62,7 @@ class Asset(object):
             length = len(self.code)
             pad_length = 4 - length if length <= 4 else 12 - length
             x.assetCode = bytearray(self.code, 'ascii') + b'\x00' * pad_length
-            x.issuer = account_xdr_object(self.issuer)
+            x.issuer = Keypair.from_public_key(self.issuer).xdr_account_id()
         if length <= 4:
             xdr_type = Xdr.const.ASSET_TYPE_CREDIT_ALPHANUM4
             return Xdr.types.Asset(type=xdr_type, alphaNum4=x)
@@ -110,52 +70,17 @@ class Asset(object):
             xdr_type = Xdr.const.ASSET_TYPE_CREDIT_ALPHANUM12
             return Xdr.types.Asset(type=xdr_type, alphaNum12=x)
 
-    def xdr(self):
-        """Create an base64 encoded XDR string for this :class:`Asset`.
-
-        :return str: A base64 encoded XDR object representing this
-            :class:`Asset`.
-
-        """
-        asset = Xdr.StellarXDRPacker()
-        asset.pack_Asset(self.to_xdr_object())
-        return base64.b64encode(asset.get_buffer())
-
     @classmethod
     def from_xdr_object(cls, asset_xdr_object):
-        """Create a :class:`Asset` from an XDR Asset object.
-
-        :param asset_xdr_object: The XDR Asset object.
-
-        :return: A new :class:`Asset` object from the given XDR Asset object.
-        """
         if asset_xdr_object.type == Xdr.const.ASSET_TYPE_NATIVE:
             return Asset.native()
         elif asset_xdr_object.type == Xdr.const.ASSET_TYPE_CREDIT_ALPHANUM4:
-            issuer = encode_check(
-                'account', asset_xdr_object.alphaNum4.issuer.ed25519).decode()
+            issuer = StrKey.encode_ed25519_public_key(asset_xdr_object.alphaNum4.issuer.ed25519)
             code = asset_xdr_object.alphaNum4.assetCode.decode().rstrip('\x00')
         else:
-            issuer = encode_check(
-                'account',
-                asset_xdr_object.alphaNum12.issuer.ed25519).decode()
-            code = (
-                asset_xdr_object.alphaNum12.assetCode.decode().rstrip('\x00'))
+            issuer = StrKey.encode_ed25519_public_key(asset_xdr_object.alphaNum12.issuer.ed25519)
+            code = asset_xdr_object.alphaNum12.assetCode.decode().rstrip('\x00')
         return cls(code, issuer)
 
-    @classmethod
-    def from_xdr(cls, xdr):
-        """Create an :class:`Asset` object from its base64 encoded XDR
-        representation.
-
-        :param bytes xdr: The base64 encoded XDR Asset object.
-
-        :return: A new :class:`Asset` object from its encoded XDR
-            representation.
-
-        """
-        xdr_decoded = base64.b64decode(xdr)
-        asset = Xdr.StellarXDRUnpacker(xdr_decoded)
-        asset_xdr_object = asset.unpack_Asset()
-        asset = Asset.from_xdr_object(asset_xdr_object)
-        return asset
+    def __eq__(self, asset):
+        return self.code == asset.code and self.issuer == asset.issuer
--- tests/test_asset.py
+++ tests/test_asset.py
@@ -1,58 +1,134 @@
-# coding: utf-8
-from unittest import TestCase
 import pytest
 
-from stellar_base.asset import Asset
-from stellar_base.exceptions import StellarAddressInvalidError, AssetCodeInvalidError
-from stellar_base.stellarxdr import Xdr
+from stellar_sdk import stellarxdr
+from stellar_sdk.asset import Asset
+from stellar_sdk.exceptions import AssetIssuerInvalidError, AssetCodeInvalidError
+from stellar_sdk.keypair import Keypair
 
 
-class TestAsset(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.source = (
-            'GDJVFDG5OCW5PYWHB64MGTHGFF57DRRJEDUEFDEL2SLNIOONHYJWHA3Z')
-        cls.cny = Asset('CNY', cls.source)
+class TestAsset:
+    @pytest.mark.parametrize('code', [
+        'XCN',
+        'Banana'
+    ])
+    def test_non_native_asset_without_issuer_raise(self, code):
+        with pytest.raises(AssetIssuerInvalidError, match="The issuer cannot be `None` except for the native asset."):
+            Asset(code)
 
-    def test_native(self):
-        assert 'XLM' == Asset.native().code
-        assert Asset.native().issuer is None
-        assert 'native' == Asset.native().type
+    @pytest.mark.parametrize('code', [
+        "",
+        "1234567890123",
+        "ab_"
+    ])
+    def test_invalid_code(self, code):
+        with pytest.raises(AssetCodeInvalidError,
+                           match=r'Asset code is invalid \(maximum alphanumeric, 12 characters at max\).'):
+            Asset(code, "GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS674JZ")
 
-    def test_is_native(self):
-        native = Asset('XLM')
-        assert native.is_native()
-        assert not self.cny.is_native()
+    @pytest.mark.parametrize('issuer', [
+        "",
+        "GCEZWKCA5",
+        "GCEZWKCA5VLDNRLN3RPRJMRZOX3Z6G5CHCGSNFHEYVXM3XOJMDS67BAD",
 
-    def test_to_xdr_object(self):
-        assert isinstance(self.cny.to_xdr_object(), Xdr.types.Asset)
+    ])
+    def test_invalid_issuer(self, issuer):
+        with pytest.raises(AssetIssuerInvalidError,
+                           match="The issuer should be a correct public key."):
+            Asset('XCN', issuer)
 
-    def test_too_long(self):
-        with pytest.raises(AssetCodeInvalidError, match='Asset code is invalid'):
-            Asset('123456789012TooLong', self.source)
-
-    def test_no_issuer(self):
-        with pytest.raises(StellarAddressInvalidError, match='Issuer cannot be `None` except native asset.'):
-            Asset('beer', None)
-
-    def test_bad_issuer(self):
-        with pytest.raises(StellarAddressInvalidError, match='Invalid issuer account: BADADDRESS'):
-            Asset('beer', 'BADADDRESS')
-
-    def test_xdr(self):
-        xdr = b'AAAAAUNOWQAAAAAA01KM3XCt1+LHD7jDTOYpe/HGKSDoQoyL1JbUOc0+E2M='
-        assert xdr == self.cny.xdr()
-
-    def test_unxdr(self):
-        xdr = self.cny.xdr()
-        cny_x = Asset.from_xdr(xdr)
-        assert self.cny == cny_x
-
-    def test_asset_to_dict(self):
-        native = Asset('XLM')
-        assert native.to_dict() == {'code': 'XLM', 'type': 'native'}
-        assert self.cny.to_dict() == {
-            'code': 'CNY',
-            'issuer': self.source,
-            'type': 'credit_alphanum4'
+    def test_native_asset(self):
+        asset_1 = Asset('XLM')
+        asset_2 = Asset.native()
+        assert asset_1 == asset_2
+        assert asset_1.code == 'XLM'
+        assert asset_1.issuer is None
+        assert asset_1.type == 'native'
+        assert asset_1.to_dict() == {
+            'type': 'native'
         }
+
+    def test_credit_alphanum4_asset(self):
+        code = 'XCN'
+        issuer = 'GCNY5OXYSY4FKHOPT2SPOQZAOEIGXB5LBYW3HVU3OWSTQITS65M5RCNY'
+        type = 'credit_alphanum4'
+        asset = Asset(code, issuer)
+        assert asset.code == code
+        assert asset.issuer == issuer
+        assert asset.type == type
+        assert asset.to_dict() == {
+            'type': type,
+            'code': code,
+            'issuer': issuer
+        }
+
+    def test_credit_alphanum12_asset(self):
+        code = 'Banana'
+        issuer = 'GCNY5OXYSY4FKHOPT2SPOQZAOEIGXB5LBYW3HVU3OWSTQITS65M5RCNY'
+        type = 'credit_alphanum12'
+        asset = Asset(code, issuer)
+        assert asset.code == code
+        assert asset.issuer == issuer
+        assert asset.type == type
+        assert asset.to_dict() == {
+            'type': type,
+            'code': code,
+            'issuer': issuer
+        }
+
+    def test_set_type_raise(self):
+        asset = Asset.native()
+        with pytest.raises(AttributeError, match="Asset type is immutable."):
+            asset.type = 'credit_alphanum4'
+
+    def test_equals(self):
+        assert Asset('XCN', 'GCNY5OXYSY4FKHOPT2SPOQZAOEIGXB5LBYW3HVU3OWSTQITS65M5RCNY') \
+               == Asset('XCN', 'GCNY5OXYSY4FKHOPT2SPOQZAOEIGXB5LBYW3HVU3OWSTQITS65M5RCNY')
+
+    @pytest.mark.parametrize('asset, xdr', [
+        pytest.param(Asset.native(), b'AAAAAA==', id='native'),
+        pytest.param(Asset('XCN', 'GCNY5OXYSY4FKHOPT2SPOQZAOEIGXB5LBYW3HVU3OWSTQITS65M5RCNY'),
+                     b'AAAAAVhDTgAAAAAAm466+JY4VR3PnqT3QyBxEGuHqw4ts9abdaU4InL3Wdg=', id='alphanum4'),
+        pytest.param(Asset('CATCOIN', 'GDJVFDG5OCW5PYWHB64MGTHGFF57DRRJEDUEFDEL2SLNIOONHYJWHA3Z'),
+                     b'AAAAAkNBVENPSU4AAAAAAAAAAADTUozdcK3X4scPuMNM5il78cYpIOhCjIvUltQ5zT4TYw==', id='alphanum12'),
+    ])
+    def test_to_xdr_object(self, asset, xdr):
+        assert asset.to_xdr_object().to_xdr() == xdr
+
+    def test_from_xdr_object_native(self):
+        xdr_type = stellarxdr.Xdr.const.ASSET_TYPE_NATIVE
+        xdr = stellarxdr.Xdr.types.Asset(type=xdr_type)
+
+        asset = Asset.from_xdr_object(xdr)
+        assert asset.is_native()
+
+    def test_from_xdr_object_alphanum4(self):
+        code = 'XCN'
+        issuer = 'GCNY5OXYSY4FKHOPT2SPOQZAOEIGXB5LBYW3HVU3OWSTQITS65M5RCNY'
+        type = 'credit_alphanum4'
+
+        x = stellarxdr.Xdr.nullclass()
+        x.assetCode = bytearray(code, 'ascii') + b'\x00'
+        x.issuer = Keypair.from_public_key(issuer).xdr_account_id()
+        xdr_type = stellarxdr.Xdr.const.ASSET_TYPE_CREDIT_ALPHANUM4
+        xdr = stellarxdr.Xdr.types.Asset(type=xdr_type, alphaNum4=x)
+
+        asset = Asset.from_xdr_object(xdr)
+        assert asset.code == code
+        assert asset.issuer == issuer
+        assert asset.type == type
+
+    def test_from_xdr_object_alphanum12(self):
+        code = 'Banana'
+        issuer = 'GCNY5OXYSY4FKHOPT2SPOQZAOEIGXB5LBYW3HVU3OWSTQITS65M5RCNY'
+        type = 'credit_alphanum12'
+
+        x = stellarxdr.Xdr.nullclass()
+        x.assetCode = bytearray(code, 'ascii') + b'\x00' * 6
+        x.issuer = Keypair.from_public_key(issuer).xdr_account_id()
+        xdr_type = stellarxdr.Xdr.const.ASSET_TYPE_CREDIT_ALPHANUM12
+        xdr = stellarxdr.Xdr.types.Asset(type=xdr_type, alphaNum12=x)
+
+        asset = Asset.from_xdr_object(xdr)
+        assert asset.code == code
+        assert asset.issuer == issuer
+        assert asset.type == type
