6059fe36d4ed5cee155ed0811743c2df07ff35ff
Jun Luo <4catcode@gmail.com>
2019-07-31 20:10:29 +0800

    feat: Network

--- stellar_sdk/network.py
+++ stellar_sdk/network.py
@@ -1,43 +1,21 @@
-# coding: utf-8
-from .utils import xdr_hash
+from .hashing import hash256
 
-NETWORKS = {
+
+class Network:
+    def __init__(self, network_passphrase: str) -> None:
+        self.network_passphrase = network_passphrase
+
+    def network_id(self) -> bytes:
+        return hash256(self.network_passphrase.encode())
+
+    def __eq__(self, other: 'Network') -> bool:
+        return self.network_passphrase == other.network_passphrase
+
+
+_network_passphrases = {
     'PUBLIC': 'Public Global Stellar Network ; September 2015',
     'TESTNET': 'Test SDF Network ; September 2015'
 }
 
-
-class Network(object):
-    """The :class:`Network` object, which represents a Stellar network.
-
-    This class represents such a stellar network such as the public livenet and
-    the Stellar Development Foundation Test network.
-
-    :param str passphrase: The passphrase for the network. (ex. 'Public Global Stellar Network ; September 2015')
-
-    """
-
-    def __init__(self, passphrase=None):
-        if passphrase is None:
-            self.passphrase = NETWORKS['TESTNET']
-        else:
-            self.passphrase = passphrase
-
-    def network_id(self):
-        """Get the network ID of the network.
-
-        Get the network ID of the network, which is an XDR hash of the
-        passphrase.
-
-        """
-        return xdr_hash(self.passphrase.encode())
-
-
-def test_network():
-    """Get the :class:`Network` representing the Test Network."""
-    return Network(NETWORKS['TESTNET'])
-
-
-def live_network():
-    """Get the :class:`Network` representing the live Network."""
-    return Network(NETWORKS['PUBLIC'])
+PUBLIC = Network(_network_passphrases['PUBLIC'])
+TESTNET = Network(_network_passphrases['TESTNET'])
