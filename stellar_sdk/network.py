8db3674045eb93bc17b2122a69031a6d0a9a1fb2
overcat <4catcode@gmail.com>
2019-07-31 22:03:59 +0800

    docs: network

--- stellar_sdk/network.py
+++ stellar_sdk/network.py
@@ -2,18 +2,40 @@ from .utils import sha256
 
 
 class Network:
+    """The :class:`Network` object, which represents a Stellar network.
+
+    This class represents such a stellar network such as the Public network and the Test network.
+
+    :param str network_passphrase: The passphrase for the network.
+        (ex. 'Public Global Stellar Network ; September 2015')
+
+    """
+
     def __init__(self, network_passphrase: str) -> None:
         self.network_passphrase = network_passphrase
 
     def network_id(self) -> bytes:
+        """Get the network ID of the network, which is an XDR hash of the
+        passphrase.
+
+        :returns: The network ID of the network.
+        """
         return sha256(self.network_passphrase.encode())
 
     def __eq__(self, other: 'Network') -> bool:
+        if not isinstance(other, Network):
+            return False
         return self.network_passphrase == other.network_passphrase
 
 
 PUBLIC_NETWORK_PASSPHRASE: str = 'Public Global Stellar Network ; September 2015'
+"""Get the Public network passphrase."""
+
 TESTNET_NETWORK_PASSPHRASE: str = 'Test SDF Network ; September 2015'
+"""Get the Test network passphrase."""
 
 PUBLIC: Network = Network(PUBLIC_NETWORK_PASSPHRASE)
+"""Get the :class:`Network` representing the PUBLIC Network."""
+
 TESTNET: Network = Network(TESTNET_NETWORK_PASSPHRASE)
+"""Get the :class:`Network` representing the Test Network."""
