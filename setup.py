d5fca11a163ba062c649c36c8b12bb2dd0a4dc82
overcat <4catcode@gmail.com>
2019-08-04 17:46:32 +0800

    fix: description

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ about = {}
 with open(os.path.join(here, "stellar_sdk", "__version__.py"), mode="r", encoding="utf-8") as f:
     exec(f.read(), about)
 
-with open("README.md", mode="r", encoding="utf-8") as f:
+with open("README.rst", mode="r", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
@@ -20,7 +20,6 @@ setup(
     version=about["__version__"],
     description=about["__description__"],
     long_description=readme,
-    long_description_content_type="text/markdown",
     author=about["__author__"],
     author_email=about["__author_email__"],
     url=about["__url__"],
