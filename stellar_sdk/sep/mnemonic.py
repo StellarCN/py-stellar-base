2132a4b7b51c52b59f2b4d936eeae972f92b35ed
overcat <4catcode@gmail.com>
2019-10-16 22:10:51 +0800

    test: sep0005 and others

--- stellar_sdk/sep/mnemonic.py
+++ stellar_sdk/sep/mnemonic.py
@@ -55,7 +55,7 @@ class StellarMnemonic(Mnemonic):
     def generate(self, strength: int = 128) -> str:
         if strength not in (128, 160, 192, 224, 256):
             raise ValueError(
-                "Strength should be one of the following [128, 160, 192, 224, 256], but it is not (%d)."
+                "Strength should be one of the following (128, 160, 192, 224, 256), but it is not (%d)."
                 % strength
             )
         return self.to_mnemonic(os.urandom(strength // 8))
