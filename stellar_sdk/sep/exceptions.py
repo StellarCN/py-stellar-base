4fbeee9d671452006a94abf3c84b4b12d752ff1f
Jun Luo <4catcode@gmail.com>
2019-12-12 20:27:51 +0800

    feat: add support for SEP-0001 (stellar.toml). (#244)

--- stellar_sdk/sep/exceptions.py
+++ stellar_sdk/sep/exceptions.py
@@ -1,5 +1,9 @@
 from ..exceptions import SdkError
 
 
+class StellarTomlNotFoundError(SdkError):
+    """If the SEP 0010 toml file not found, the exception will be thrown."""
+
+
 class InvalidSep10ChallengeError(SdkError):
     """If the SEP 0010 validation fails, the exception will be thrown."""
