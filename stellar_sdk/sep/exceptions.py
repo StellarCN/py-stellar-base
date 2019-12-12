1a010d6612ad3e1f5bd1fc693567fce1b707198e
overcat <4catcode@gmail.com>
2019-12-12 22:48:32 +0800

    feat: add support for SEP-0002 (Federation protocol).

--- stellar_sdk/sep/exceptions.py
+++ stellar_sdk/sep/exceptions.py
@@ -1,8 +1,28 @@
-from ..exceptions import SdkError
+from ..exceptions import SdkError, BaseRequestError
 
 
 class StellarTomlNotFoundError(SdkError):
     """If the SEP 0010 toml file not found, the exception will be thrown."""
+
+
+class InvalidFederationAddress(SdkError):
+    """If the federation address is invalid, the exception will be thrown."""
+
+
+class FederationServerNotFoundError(SdkError):
+    """If the federation address is invalid, the exception will be thrown."""
+
+
+class BadFederationResponseError(BaseRequestError):
+    """If the federation address is invalid, the exception will be thrown.
+
+    :param response: client response
+    """
+
+    def __init__(self, response) -> None:
+        super().__init__(response)
+        self.message: str = response.text
+        self.status: int = response.status_code
 
 
 class InvalidSep10ChallengeError(SdkError):
