fc6c044d562e0fe2bfffc019e9243d4259534a18
overcat <4catcode@gmail.com>
2020-05-05 21:03:19 +0800

    example: build fee bump transaction and pay to muxed account.

--- examples/build_fee_bump_transaction.py
+++ examples/build_fee_bump_transaction.py
@@ -1,25 +1,36 @@
-from stellar_sdk.account import Account
-from stellar_sdk.keypair import Keypair
-from stellar_sdk.network import Network
-from stellar_sdk.transaction_builder import TransactionBuilder
+import pprint
 
-inner_keypair = Keypair.from_secret(
-    "SBKTIFHJSS3JJWEZO2W74DZSA45WZU56LOL3AY7GAW63BXPEJQFYV53E"
-)
-inner_source = Account(inner_keypair.public_key, 7)
-destination = "GDQERENWDDSQZS7R7WKHZI3BSOYMV3FSWR7TFUYFTKQ447PIX6NREOJM"
-amount = "2000"
+from stellar_sdk import Keypair, Server, TransactionBuilder, Network
+from stellar_sdk.exceptions import BadRequestError
+
+horizon_url = "http://horizon-testnet.stellar.org/"
+network_passphrase = Network.TESTNET_NETWORK_PASSPHRASE
+
+fee_source_keypair = Keypair.from_secret("SASZKBDB6PFHXN6LRH4NQNTRGLGDTI3PSUVIKMZMLTYYBB7NDVMA6DSL")
+inner_source_keypair = Keypair.from_secret("SC5O7VZUXDJ6JBDSZ74DSERXL7W3Y5LTOAMRF7RQRL3TAGAPS7LUVG3L")
+destination_address = "GBVKI23OQZCANDUZ2SI7XU7W6ICYKYT74JBXDD2CYRDAFZHZNRPASSQK"
+
+server = Server(horizon_url=horizon_url)
+inner_account = server.load_account(inner_source_keypair)
+
 inner_tx = (
-    TransactionBuilder(inner_source, Network.TESTNET_NETWORK_PASSPHRASE, 200, v1=True)
-    .append_payment_op(destination=destination, amount=amount, asset_code="XLM")
-    .build()
+    TransactionBuilder(source_account=inner_account, network_passphrase=network_passphrase, base_fee=50, v1=True)
+        .append_payment_op(destination=destination_address, amount="100", asset_code="XLM")
+        .build()
 )
-inner_tx.sign(inner_keypair)
-fee_source = Keypair.from_secret(
-    "SB7ZMPZB3YMMK5CUWENXVLZWBK4KYX4YU5JBXQNZSK2DP2Q7V3LVTO5V"
-)
-base_fee = 200
+inner_tx.sign(inner_source_keypair)
+
+try:
+    # This transaction will fail.
+    tx_insufficient_fee_resp = server.submit_transaction(inner_tx)
+except BadRequestError as e:
+    print(e)
+
 fee_bump_tx = TransactionBuilder.build_fee_bump_transaction(
-    fee_source.public_key, base_fee, inner_tx, Network.TESTNET_NETWORK_PASSPHRASE,
+    fee_source=fee_source_keypair,
+    base_fee=200,
+    inner_transaction_envelope=inner_tx, network_passphrase=network_passphrase
 )
-fee_bump_tx.sign(fee_source)
+fee_bump_tx.sign(fee_source_keypair)
+response = server.submit_transaction(fee_bump_tx)
+pprint.pprint(response)
