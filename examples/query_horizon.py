0d0baaac3e18778caa1915975d80fd845907ddb2
overcat <4catcode@gmail.com>
2020-05-10 10:31:58 +0800

    example: update example to v3.

--- examples/query_horizon.py
+++ examples/query_horizon.py
@@ -1,15 +1,17 @@
 from stellar_sdk import Server
 
-server = Server(horizon_url="https://horizon-testnet.stellar.org")
+server = Server(horizon_url="https://horizon.stellar.org")
 
-# get a list of transactions that occurred in ledger 1400
-transactions = server.transactions().for_ledger(1400).call()
-print(transactions)
+# get a list of transactions that occurred in ledger 29576671
+transactions = server.transactions().for_ledger(29576671).call()
+# You can use raw data or parsed data to get a better development experience.
+# print(transactions.raw_data)
+print(transactions.parse_data())
 
 # get a list of transactions submitted by a particular account
 transactions = (
     server.transactions()
-    .for_account(account_id="GASOCNHNNLYFNMDJYQ3XFMI7BYHIOCFW3GJEOWRPEGK2TDPGTG2E5EDW")
+    .for_account(account_id="GDZP7ZPAR4UBJIZH5EGAKJADTKX3LIXKBIPZFFN54XHB6XBAK4QBOHRE")
     .call()
 )
 # You can use raw data or parsed data to get a better development experience.
