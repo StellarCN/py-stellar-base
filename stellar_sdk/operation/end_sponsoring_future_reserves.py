2acc2176857f1a66c7293200649a1779596c40ba
overcat <4catcode@gmail.com>
2020-09-22 23:59:20 +0800

    WIP

--- stellar_sdk/operation/end_sponsoring_future_reserves.py
+++ stellar_sdk/operation/end_sponsoring_future_reserves.py
@@ -1,2 +1,28 @@
-class EndSponsoringFutureReserves:
-    pass
+from .operation import Operation
+from ..xdr import Xdr
+
+
+class EndSponsoringFutureReserves(Operation):
+    def __init__(self, source: str = None) -> None:
+        super().__init__(source)
+
+    @classmethod
+    def type_code(cls) -> int:
+        return Xdr.const.END_SPONSORING_FUTURE_RESERVES
+
+    def _to_operation_body(self) -> Xdr.nullclass:
+        body = Xdr.nullclass()
+        body.type = Xdr.const.END_SPONSORING_FUTURE_RESERVES
+        return body
+
+    @classmethod
+    def from_xdr_object(
+            cls, operation_xdr_object: Xdr.types.Operation
+    ) -> "EndSponsoringFutureReserves":
+        """Creates a :class:`EndSponsoringFutureReserves` object from an XDR Operation
+        object.
+        """
+        source = Operation.get_source_from_xdr_obj(operation_xdr_object)
+        op = cls(source=source, )
+        op._source_muxed = Operation.get_source_muxed_from_xdr_obj(operation_xdr_object)
+        return op
