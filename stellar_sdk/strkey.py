d9cf24e510504454d2d7f5e7b87cc31816f08954
Jun Luo <4catcode@gmail.com>
2021-07-29 17:28:17 +0800

    add memo note

--- stellar_sdk/strkey.py
+++ stellar_sdk/strkey.py
@@ -293,6 +293,7 @@ def _bytes_from_decode_data(s: Union[str, bytes, bytearray]) -> bytes:
 
 
 def _calculate_checksum(payload):
+    # memo note: https://gist.github.com/manran/a8357808ef71415d266dc64f0079f298
     # This code calculates CRC16-XModem checksum of payload
     checksum = binascii.crc_hqx(payload, 0)
     # Ensure that the checksum is in LSB order.
