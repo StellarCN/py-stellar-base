6515f26afbd01db8fbd6bd9b31c392e78c500ef1
Jun Luo <4catcode@gmail.com>
2021-09-15 22:41:54 +0800

    example: update comment for amm

--- examples/amm.py
+++ examples/amm.py
@@ -18,10 +18,10 @@ source_keypair = Keypair.from_secret(
     "SBLPQEGODE2GKGL2RMQRZAJBR73S3R4UI2RD2I2U2ZDDIVU2NDBSZATS"
 )
 
-# Here we define a Liquidity Pool Asset
-asset_a = Asset.native()
 # We assume that Hello asset already exists in the Stellar network,
 # and you can learn how to issue assets through examples/issue_asset.py
+# Here we define a Liquidity Pool Asset.
+asset_a = Asset.native()
 asset_b = Asset("Hello", "GD5Y3PMKI46MPILDG4OQP4SGFMRNKYEPJVDAPR3P3I2BMZ3O7IX6DB2Y")
 liquidity_pool_asset = LiquidityPoolAsset(asset_a=asset_a, asset_b=asset_b)
 liquidity_pool_id = liquidity_pool_asset.liquidity_pool_id
@@ -29,7 +29,7 @@ print(f"Liquidity Pool ID: {liquidity_pool_id}")
 
 source_account = server.load_account(account_id=source_keypair.public_key)
 
-# First we need to add a line of trust for liquidity_pool_asset.
+# First we need to add a trust line for liquidity_pool_asset.
 transaction1 = (
     TransactionBuilder(
         source_account=source_account,
