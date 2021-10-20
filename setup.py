af21431e35df4385a4494bce6c2acb1ecbca14dd
Jun Luo <4catcode@gmail.com>
2021-10-20 08:01:21 +0800

    feat: added a `py.typed` file so that mypy will know to use our type annotations. (#543)

--- setup.py
+++ setup.py
@@ -63,5 +63,6 @@ setup(
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Software Development :: Libraries :: Python Modules",
+        "Typing :: Typed",
     ],
 )
