c459dadf8ea1a37f04ff65ef89dc8db8dffc3826
overcat <4catcode@gmail.com>
2023-02-17 16:20:58 +0800

    feat: add support for the newly added RPC interface in Soroban Preview 7

--- stellar_sdk/soroban/server.py
+++ stellar_sdk/soroban/server.py
@@ -37,58 +37,95 @@ class SorobanServer:
         return self._post(request, GetHealthResponse)
 
     def get_account(self, account_id: str) -> GetAccountResponse:
-        request = Request[List[str]](
+        request = Request[GetAccountRequest](
             id=_generate_unique_request_id(),
             method="getAccount",
-            params=[account_id],
+            params=GetAccountRequest(address=account_id),
         )
         return self._post(request, GetAccountResponse)
 
-    def get_contract_data(
-        self, contract_id: str, key: stellar_xdr.SCVal
-    ) -> GetContractDataResponse:
-        request = Request[List[str]](
-            id=_generate_unique_request_id(),
-            method="getContractData",
-            params=[contract_id, key.to_xdr()],
+    def get_events(
+        self,
+        start_ledger: int,
+        end_ledger: int,
+        filters: List[EventFilter] = None,
+        cursor: str = None,
+        limit: int = None,
+    ) -> GetEventsResponse:
+        pagination = PaginationOptions(cursor=cursor, limit=limit)
+        data = GetEventsRequest(
+            startLedger=start_ledger,
+            endLedger=end_ledger,
+            filters=filters,
+            pagination=pagination,
         )
-        return self._post(request, GetContractDataResponse)
+        request: Request = Request[GetEventsRequest](
+            id=_generate_unique_request_id(), method="getEvents", params=data
+        )
+        return self._post(request, GetEventsResponse)
+
+    def get_network(self) -> GetNetworkResponse:
+        request: Request = Request(
+            id=_generate_unique_request_id(),
+            method="getNetwork",
+        )
+        return self._post(request, GetNetworkResponse)
+
+    def get_ledger_entry(self, key: stellar_xdr.LedgerKey) -> GetLedgerEntryResponse:
+        # TODO: Split it into multiple points
+        request = Request[GetLedgerEntryRequest](
+            id=_generate_unique_request_id(),
+            method="getLedgerEntry",
+            params=GetLedgerEntryRequest(key=key.to_xdr()),
+        )
+        return self._post(request, GetLedgerEntryResponse)
 
     def get_transaction_status(
         self, transaction_hash: str
     ) -> GetTransactionStatusResponse:
-        request = Request[List[str]](
+        request = Request[GetTransactionStatusRequest](
             id=_generate_unique_request_id(),
             method="getTransactionStatus",
-            params=[transaction_hash],
+            params=GetTransactionStatusRequest(hash=transaction_hash),
         )
         return self._post(request, GetTransactionStatusResponse)
 
     def simulate_transaction(
-        self, transaction_envelope: TransactionEnvelope
+        self, transaction_envelope: Union[TransactionEnvelope, str]
     ) -> SimulateTransactionResponse:
-        request = Request[List[str]](
+        xdr = (
+            transaction_envelope.to_xdr()
+            if isinstance(transaction_envelope, TransactionEnvelope)
+            else transaction_envelope
+        )
+        request = Request[SimulateTransactionRequest](
             id=_generate_unique_request_id(),
             method="simulateTransaction",
-            params=[transaction_envelope.to_xdr()],
+            params=SimulateTransactionRequest(transaction=xdr),
         )
         # TODO: error? request.error is None, request.result.error is not None
         return self._post(request, SimulateTransactionResponse)
 
     def send_transaction(
-        self, transaction_envelope: TransactionEnvelope
+        self, transaction_envelope: Union[TransactionEnvelope, str]
     ) -> SendTransactionResponse:
-        request = Request[List[str]](
+        xdr = (
+            transaction_envelope.to_xdr()
+            if isinstance(transaction_envelope, TransactionEnvelope)
+            else transaction_envelope
+        )
+        request = Request[SendTransactionRequest](
             id=_generate_unique_request_id(),
             method="sendTransaction",
-            params=[transaction_envelope.to_xdr()],
+            params=SendTransactionRequest(transaction=xdr),
         )
         return self._post(request, SendTransactionResponse)
 
     def _post(self, request_body: Request, response_body_type: Type[V]) -> V:
+        json_data = request_body.dict(by_alias=True)
         data = self._client.post(
             self.server_url,
-            json_data=request_body.dict(),
+            json_data=json_data,
         )
         response = Response[response_body_type, str].parse_obj(data.json())  # type: ignore[valid-type]
         if response.error:
--- stellar_sdk/soroban/soroban_rpc.py
+++ stellar_sdk/soroban/soroban_rpc.py
@@ -1,61 +1,157 @@
+from datetime import datetime
 from enum import Enum
-from typing import Optional, List
+from typing import Optional, List, Dict, Any
 
 from pydantic import BaseModel, Field
 
+from ..xdr.sc_val import SCVal as XdrSCVal
 
+
+# account
+class GetAccountRequest(BaseModel):
+    address: str
+
+
+class GetAccountResponse(BaseModel):
+    id: str
+    sequence: int
+
+
+# get_events
+class SegmentFilter(BaseModel):
+    wildcard: Optional[str]
+    scval: Optional[XdrSCVal]
+
+    class Config:
+        json_encoders = {
+            XdrSCVal: lambda v: v.to_xdr(),
+        }
+        arbitrary_types_allowed = True
+
+
+class EventFilter(BaseModel):
+    event_type: Optional[str] = Field(alias="type")
+    contract_ids: Optional[List[str]] = Field(alias="contractIds")
+    topics: Optional[List[List[SegmentFilter]]]
+
+
+class EventInfoValue(BaseModel):
+    xdr: str
+
+
+class EventInfo(BaseModel):
+    event_type: str = Field(alias="type")
+    ledger: int = Field(alias="ledger")
+    ledger_close_at: datetime = Field(alias="ledgerClosedAt")
+    contract_id: str = Field(alias="contractId")
+    id: str = Field(alias="id")
+    paging_token: str = Field(alias="pagingToken")
+    topic: List[str] = Field(alias="topic")
+    value: EventInfoValue = Field(alias="value")
+
+
+class PaginationOptions(BaseModel):
+    cursor: Optional[str]
+    limit: Optional[int]
+
+
+class GetEventsRequest(BaseModel):
+    start_ledger: str = Field(alias="startLedger")
+    end_ledger: str = Field(alias="endLedger")  # TODO: check it
+    filters: Optional[List[EventFilter]]
+    pagination: Optional[PaginationOptions]
+
+
+class GetEventsResponse(BaseModel):
+    events: List[EventInfo] = Field(alias="events")
+    latest_ledger: int = Field(alias="latestLedger")
+
+
+# get_ledger_entry
+class GetLedgerEntryRequest(BaseModel):
+    key: str
+
+
+class GetLedgerEntryResponse(BaseModel):
+    xdr: str
+    last_modified_ledger_seq: int = Field(alias="lastModifiedLedgerSeq")
+    latest_ledger: int = Field(alias="latestLedger")
+
+
+# get_network
+class GetNetworkResponse(BaseModel):
+    friendbot_url: Optional[str] = Field(alias="friendbotUrl")
+    passphrase: str
+    protocol_version: int = Field(alias="protocolVersion")
+
+
+# health
+class GetHealthResponse(BaseModel):
+    status: str
+
+
+# simulate_transaction
+class SimulateTransactionRequest(BaseModel):
+    transaction: str
+
+
+class SimulateTransactionCost(BaseModel):
+    cpu_insns: int = Field(alias="cpuInsns")
+    mem_bytes: int = Field(alias="memBytes")
+
+
+class SimulateTransactionResult(BaseModel):
+    auth: Optional[List[str]]
+    footprint: str
+    xdr: str
+
+
+class SimulateTransactionResponse(BaseModel):
+    error: Optional[str]
+    results: Optional[List[SimulateTransactionResult]]
+    cost: SimulateTransactionCost
+    latest_ledger: int = Field(alias="latestLedger")
+
+
+# get_transaction_status
 class TransactionStatus(Enum):
     PENDING = "pending"
     SUCCESS = "success"
     ERROR = "error"
 
 
-class Cost(BaseModel):
-    cpu_insns: str = Field(alias="cpuInsns")
-    mem_bytes: str = Field(alias="memBytes")
+class TransactionResponseError(BaseModel):
+    code: str
+    message: str
+    data: Dict[str, Any]
 
 
-class TransactionStatusResult(BaseModel):
+class SCVal(BaseModel):
     xdr: str
 
 
-class Footprint(BaseModel):
-    read_only: List[str]
-    read_write: List[str]
-
-
-class GetAccountResponse(BaseModel):
-    id: str
-    sequence: int
-    # balances: List[Balance] # TODO: implement
-
-
-class GetHealthResponse(BaseModel):
-    status: str
-
-
-class GetContractDataResponse(BaseModel):
-    xdr: str
-    last_modified_ledger_seq: Optional[int] = Field(alias="lastModifiedLedgerSeq")
-    latest_ledger: Optional[int] = Field(alias="latestLedger")
+class GetTransactionStatusRequest(BaseModel):
+    hash: str
 
 
 class GetTransactionStatusResponse(BaseModel):
     id: str
     status: TransactionStatus
-    results: Optional[List[TransactionStatusResult]]
-    # error: Optional[jsonrpc.Error[E]]
+    envelope_xdr: Optional[str] = Field(alias="envelopeXdr")
+    result_xdr: Optional[str] = Field(alias="resultXdr")
+    result_meta_xdr: Optional[str] = Field(alias="resultMetaXdr")
+    results: Optional[List[SCVal]]
+    # error will be empty unless status is equal to "error"
+    error: Optional[TransactionResponseError]
+
+
+# send_transaction
+class SendTransactionRequest(BaseModel):
+    transaction: str
 
 
 class SendTransactionResponse(BaseModel):
     id: str
     status: TransactionStatus
-    # error: Optional[jsonrpc.Error[E]]
-
-
-class SimulateTransactionResponse(BaseModel):
-    footprint: str
-    cost: Cost
-    results: Optional[List[TransactionStatusResult]]
-    error: Optional[str]
-    latest_ledger: int = Field(alias="latestLedger")
+    # error will be empty unless status is equal to "error"
+    error: Optional[TransactionResponseError]
