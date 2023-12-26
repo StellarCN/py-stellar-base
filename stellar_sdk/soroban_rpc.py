896540258756cf7b2d54302bff5dd02d74b3e5fa
overcat <4catcode@gmail.com>
2023-12-26 20:59:20 +0800

    fix: mark `GetTransactionResponse.create_at` as `Optional`.

--- stellar_sdk/soroban_rpc.py
+++ stellar_sdk/soroban_rpc.py
@@ -268,7 +268,7 @@ class GetTransactionResponse(BaseModel):
         alias="resultMetaXdr", default=None
     )  # stellar_sdk.xdr.TransactionMeta
     ledger: Optional[int] = Field(alias="ledger", default=None)
-    create_at: int = Field(alias="createdAt")
+    create_at: Optional[int] = Field(alias="createdAt", default=None)
 
 
 # send_transaction
