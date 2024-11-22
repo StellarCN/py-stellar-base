3471308e30900af1867237347ed290ee90aed9e8
Jun Luo <4catcode@gmail.com>
2024-11-22 11:51:15 +0800

    WIP

--- stellar_sdk/operation/invoke_host_function.py
+++ stellar_sdk/operation/invoke_host_function.py
@@ -1,9 +1,7 @@
 from typing import List, Optional, Sequence, Union
 
 from .. import xdr as stellar_xdr
-from ..address import Address
 from ..muxed_account import MuxedAccount
-from ..strkey import StrKey
 from .operation import Operation
 
 __all__ = ["InvokeHostFunction"]
@@ -60,42 +58,6 @@ class InvokeHostFunction(Operation):
             auth=auth,
             source=source,
         )
-
-    @classmethod
-    def invoke_contract_function(
-        cls,
-        contract_id: str,
-        function_name: str,
-        parameters: Sequence[stellar_xdr.SCVal] = None,
-        auth: Sequence[stellar_xdr.SorobanAuthorizationEntry] = None,
-        source: Optional[Union[MuxedAccount, str]] = None,
-    ) -> "InvokeHostFunction":
-        """Create a new :class:`InvokeHostFunction` operation.
-
-        :param contract_id: The ID of the contract to invoke.
-        :param function_name: The name of the function to invoke.
-        :param parameters: The parameters to pass to the method.
-        :param auth: The authorizations required to execute the host function.
-        :param source: The source account for the operation. Defaults to the
-            transaction's source account.
-        """
-        if not StrKey.is_valid_contract(contract_id):
-            raise ValueError("`contract_id` is invalid.")
-
-        if parameters is None:
-            parameters = []
-
-        host_function = stellar_xdr.HostFunction(
-            stellar_xdr.HostFunctionType.HOST_FUNCTION_TYPE_INVOKE_CONTRACT,
-            invoke_contract=stellar_xdr.InvokeContractArgs(
-                contract_address=Address(contract_id).to_xdr_sc_address(),
-                function_name=stellar_xdr.SCSymbol(
-                    sc_symbol=function_name.encode("utf-8")
-                ),
-                args=list(parameters),
-            ),
-        )
-        return cls(host_function=host_function, auth=auth, source=source)
 
     def __repr__(self):
         return f"<InvokeHostFunction [host_function={self.host_function}, auth={self.auth}, source={self.source}]>"
